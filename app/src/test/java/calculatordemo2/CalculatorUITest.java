3406799fc9996c4de0b168a3468df5a880e6d384
Ted Lehr <ted.lehr@txstate.edu>
2023-04-26 15:06:02 -0500

    Added test for write()

--- app/src/test/java/calculatordemo2/CalculatorUITest.java
+++ app/src/test/java/calculatordemo2/CalculatorUITest.java
@@ -3,13 +3,39 @@
  */
 package calculatordemo2;
 
+import javax.swing.JTextArea;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.Field;  // This brings in the Field feature of Java Reflection
 
 class CalculatorUITest {
-    @Test void appPanelIsCreated() {
-        CalculatorUI classUnderTest = new CalculatorUI();
-        assertNull(classUnderTest, "app should have a panel object");
+
+    private static CalculatorUI classUnderTest;
+
+    @BeforeAll
+    public static void setUp() {
+        classUnderTest = new CalculatorUI();
+    }
+
+    @DisplayName("Testing that writer writes the display")
+    @Test
+    public void writerSetText() throws Exception {
+        // Use reflection to access the private field “text”
+        Class cls = classUnderTest.getClass();
+        Field field = cls.getDeclaredField("text");
+        field.setAccessible(true);
+        // Set the value of “text” to “mytext”
+        JTextArea text = (JTextArea) field.get(classUnderTest);
+        text.setText("mytext");
+        // Test that the value of “text” is “mytext”
+        assertEquals("mytext", text.getText());
+    }
+    
+    @Test 
+    void appPanelIsCreated() {
+        assertNotNull(classUnderTest, "app should have a panel object");
     }
 }
 
